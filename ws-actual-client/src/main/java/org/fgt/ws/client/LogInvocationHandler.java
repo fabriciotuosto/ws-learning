db700ffff270417d6ae02c4157ce31eb66413b36
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2010-03-04 13:01:51 -0300

    Implementation of log invocation handler

--- ws-actual-client/src/main/java/org/fgt/ws/client/LogInvocationHandler.java
+++ ws-actual-client/src/main/java/org/fgt/ws/client/LogInvocationHandler.java
@@ -2,13 +2,33 @@ package org.fgt.ws.client;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LogInvocationHandler implements InvocationHandler{
 
+	private static final Logger LOG = LoggerFactory.getLogger(LogInvocationHandler.class);
+	
 	public Object invoke(Object proxy, Method method, Object[] args)
 			throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+		long start = System.currentTimeMillis();
+		if(LOG.isDebugEnabled()){
+			LOG.debug("Invoking method= {}(), with args= {}",method.toString(),Arrays.toString(args));
+		}
+		try{
+			return actualInvoke(method,args);
+		}finally{
+			if(LOG.isDebugEnabled()){
+				long end = System.currentTimeMillis();
+				LOG.debug("Method {}() took {}ms",Long.valueOf((end-start)));
+			}			
+		}
+	}
+
+	private Object actualInvoke(Method method, Object[] args) throws Throwable{
+		return method.invoke(null, args);
 	}
 
 }
