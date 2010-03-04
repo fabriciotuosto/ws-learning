63d6b18a38ac19965e4b7c6ced8edfe57a9f10a4
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2010-03-04 13:02:25 -0300

    implemented cache invocation handler

--- ws-actual-client/src/main/java/org/fgt/ws/client/CacheInvocationHandler.java
+++ ws-actual-client/src/main/java/org/fgt/ws/client/CacheInvocationHandler.java
@@ -3,16 +3,38 @@ package org.fgt.ws.client;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
+
 public class CacheInvocationHandler implements InvocationHandler{
 
-	public CacheInvocationHandler() {
-		
+	private final Cache cache;
+	private final Object instance;
+
+	public CacheInvocationHandler(Cache cache,Object instance) {
+		this.cache = cache;
+		this.instance = instance;
 	}
 	
 	public Object invoke(Object proxy, Method method, Object[] args)
 			throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+		Object result = retrieveFromCache(method,args);
+		if(result == null){
+			result = invokeAndCache(method, args); 
+		}
+		return result;
+	}
+	
+	private Object invokeAndCache(Method method,Object[] args) throws Throwable{
+		Object result = method.invoke(instance, args);
+		Element element = new Element("", result);
+		cache.put(element);
+		return result;
+	}
+	
+	private Object retrieveFromCache(Method method, Object[] args) {
+		String key = "";
+		return cache.get(key);
 	}
 
 }
