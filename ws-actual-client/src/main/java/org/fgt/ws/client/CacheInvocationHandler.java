42a1ac8f8b8211c26efc9ca4268fbd3d5f93c193
unknown <tuosto@.americas.hpqcorp.net>
2010-03-04 12:37:28 -0300

    Initial Commit

---
+++ ws-actual-client/src/main/java/org/fgt/ws/client/CacheInvocationHandler.java
@@ -0,0 +1,18 @@
+package org.fgt.ws.client;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CacheInvocationHandler implements InvocationHandler{
+
+	public CacheInvocationHandler() {
+		
+	}
+	
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ws-actual-client/src/main/java/org/fgt/ws/client/LogInvocationHandler.java
@@ -0,0 +1,14 @@
+package org.fgt.ws.client;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LogInvocationHandler implements InvocationHandler{
+
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
